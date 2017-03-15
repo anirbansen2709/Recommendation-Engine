9344d022cc3fa8edb585c99535395b0b20b7b1d2
anirbansen3027 <anirbansen3027@gmail.com>
2017-03-15 15:20:54 +0530

    moviesWithRatingsFromDb

--- src/main/java/com/gamma/dexter/musicRecommendation/SongsModel.java
+++ src/main/java/com/gamma/dexter/musicRecommendation/SongsModel.java
@@ -4,11 +4,24 @@ package com.gamma.dexter.musicRecommendation;
  * Created by Anirban on 08-Mar-17.
  */
 public class SongsModel {
+
     String name;
     float avgRating;
 
-    public SongsModel(String name, float avgRating) {
-        this.name=name;
-        this.avgRating= avgRating;
+    public float getAvgRating() {
+        return avgRating;
     }
+
+    public void setAvgRating(float avgRating) {
+        this.avgRating = avgRating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
