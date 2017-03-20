89fadd6acb87f9c7765c6535a82660d486692c86
Debashish Sen <dsen19@gmail.com>
2017-03-20 12:09:06 +0530

    Java Client

---
+++ src/main/java/com/gamma/dexter/test/HttpUtil.java
@@ -0,0 +1,195 @@
+package com.gamma.dexter.test;
+
+import org.apache.commons.codec.Charsets;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Debashish Sen on 17-Mar-17.
+ */
+
+public class HttpUtil {
+
+    private final static Logger logger = LoggerFactory.getLogger(HttpUtil.class);
+
+    public static String post(String url, String data) throws IOException {
+
+        HttpClient client = HttpClients.createDefault();
+        HttpPost httpost = new HttpPost(url);
+
+        httpost.setHeader("type", "POST");
+        httpost.setHeader("Accept", "application/json");
+        httpost.setHeader("Content-type", "application/json");
+        httpost.setHeader("dataType", "json");
+        httpost.setEntity(new StringEntity(data));
+
+        HttpResponse response = client.execute(httpost);
+        HttpEntity entity = response.getEntity();
+
+        if (entity != null) {
+            return EntityUtils.toString(entity);
+        }
+
+        return "";
+    }
+
+    public static String get(String url) throws IOException {
+
+        HttpClient client = HttpClients.createDefault();
+        HttpGet httGet = new HttpGet(url);
+
+        httGet.setHeader("type", "GET");
+
+        HttpResponse response = client.execute(httGet);
+        HttpEntity entity = response.getEntity();
+
+        if (entity != null) {
+            return EntityUtils.toString(entity);
+        }
+
+        return "";
+    }
+
+    public static String sendHttpPost(String url, String msg) throws IOException,
+            NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+        return sendHttpPost(url, msg, new HashMap<String, String>());
+    }
+
+    public static String sendHttpsGetWithProxy(String hostStr, String urlPart, String msg, Map<String, String> header, Map<String, String> proxyDet) throws IOException {
+
+        HttpHost proxy = new HttpHost(proxyDet.get("ip"), Integer.parseInt(proxyDet.get("port")), "http");
+
+        CloseableHttpClient httpClient = HttpClientBuilder.create().setProxy(proxy).build();
+
+        try {
+            HttpHost host = new HttpHost(hostStr, 443, "https");
+            HttpGet get = new HttpGet(urlPart);
+
+            for (String key : header.keySet()) {
+                get.setHeader(key, header.get(key));
+            }
+
+            System.out.println("executing request to " + hostStr + urlPart + " via " + proxy);
+            HttpResponse response = httpClient.execute(host, get);
+            logger.info("Response Status line : " + response.getStatusLine());
+
+            HttpEntity entity = response.getEntity();
+
+            String res = null;
+            if (entity != null) {
+                res = EntityUtils.toString(entity);
+            }
+
+            return res;
+
+        } finally {
+            httpClient.close();
+        }
+    }
+
+    public static String sendHttpsPostWithProxy(String hostStr, String urlPart, String msg, Map<String, String> header, Map<String, String> proxyDet) throws IOException {
+
+        HttpHost proxy = new HttpHost(proxyDet.get("ip"), Integer.parseInt(proxyDet.get("port")), "http");
+
+        try (CloseableHttpClient httpClient = HttpClientBuilder.create().setProxy(proxy).build()) {
+            HttpHost host = new HttpHost(hostStr, 443, "https");
+            HttpPost post = new HttpPost(urlPart);
+
+            post.setHeader("type", "POST");
+            post.setHeader("Accept", "application/json");
+            post.setHeader("Content-type", "application/json");
+            post.setHeader("dataType", "json");
+
+            for (String key : header.keySet()) {
+                post.setHeader(key, header.get(key));
+            }
+
+            post.setEntity(new StringEntity(msg, Charset.forName("UTF-8")));
+
+            System.out.println("executing request to " + hostStr + urlPart + " via " + proxy);
+            HttpResponse response = httpClient.execute(host, post);
+            logger.info("Response Status : " + response.getStatusLine());
+
+            HttpEntity entity = response.getEntity();
+
+            String res = null;
+            if (entity != null) {
+                res = EntityUtils.toString(entity);
+            }
+
+            return res;
+        }
+    }
+
+    public static String sendHttpPost(String url, String msg, Map<String, String> header) throws IOException {
+
+        logger.info("Post url -> " + url);
+
+        HttpClient client = HttpClients.createDefault();
+        HttpPost httpost = new HttpPost(url);
+
+        httpost.setHeader("type", "POST");
+        httpost.setHeader("Accept", "application/json");
+        httpost.setHeader("Content-type", "application/json");
+        httpost.setHeader("dataType", "json");
+
+        for (String key : header.keySet()) {
+            httpost.setHeader(key, header.get(key));
+        }
+
+        httpost.setEntity(new StringEntity(msg, Charset.forName("UTF-8")));
+
+        HttpResponse response = client.execute(httpost);
+
+        HttpEntity entity = response.getEntity();
+
+        String res = null;
+        if (entity != null) {
+            res = EntityUtils.toString(entity, Charsets.UTF_8);
+        }
+        return res;
+    }
+
+    public static String sendHttpGet(String url) throws IOException {
+        logger.info("Sending msg to url -> " + url);
+
+        org.apache.http.client.HttpClient client = HttpClients.createDefault();
+        org.apache.http.client.methods.HttpGet httpGet =
+                new org.apache.http.client.methods.HttpGet(url);
+
+        org.apache.http.HttpResponse response = client.execute(httpGet);
+
+        HttpEntity entity = response.getEntity();
+
+        String res = null;
+        if (entity != null) {
+            res = EntityUtils.toString(entity);
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) throws IOException {
+        String res = HttpUtil.get("http://localhost:8124/test");
+        System.out.println(res);
+    }
+}
