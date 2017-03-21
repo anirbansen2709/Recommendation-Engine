ee050e38be22decec27d34bc769ef7972c4ef1b8
Debashish Sen <dsen19@gmail.com>
2017-03-21 12:41:24 +0530

    topRatedMovies JS

---
+++ src/main/webapp/resources/app/js/topRatedMovies.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Debashish Sen on 20-Mar-17.
+ */
+
+$(document).ready(function () {
+    topRatedMovies();
+});
+
+function topRatedMovies() {
+
+        $('#loadingModal').modal('show');
+        $.ajax({
+            type: "GET",
+            dataType: "json",
+            url: "getSongsWithGenres",
+            success: function (data) {
+
+                $('#loadingModal').modal('hide');
+
+            }, error: function (data, status) {
+            }
+        });
+ }
