c6d61bf3367968d1bc7904a1ad0e78d28cad202e
anirbansen3027 <anirbansen3027@gmail.com>
2017-05-04 14:18:26 +0530

    header.js added

---
+++ src/main/webapp/resources/app/js/header.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Anirban on 03-May-17.
+ */
+$(document).ready(function () {
+    showUsername();
+});
+function showUsername(){
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: "currentUser",
+        success: function (data) {
+        console.log(data);
+            $("#userName").text(data["Payload"][0]["Username"]);
+        }, error: function (data, status) {
+        }
+    });
+}
